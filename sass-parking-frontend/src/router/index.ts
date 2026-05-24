7a91ef313e0e1b4e169f02f19e422fc5664f0bff
ozsumit <pokhrelsumit36@gmail.com>
2026-05-24 14:04:21 +0545

    Add login feature

--- sass-parking-frontend/src/router/index.ts
+++ sass-parking-frontend/src/router/index.ts
@@ -1,8 +1,9 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHistory } from "vue-router";
 
-import LoginView from "../views/auth/LoginView.vue"
-import OperatorDashboardView from "../views/operator/DashboardView.vue"
-import TenantDashboardView from "../views/tenant/TenantDashboardView.vue"
+import LoginView from "../views/auth/LoginView.vue";
+import OperatorDashboardView from "../views/operator/DashboardView.vue";
+import TenantDashboardView from "../views/tenant/TenantDashboardView.vue";
+import SuperadminDashboardView from "../views/SuperAdminDashboard/SuperadminDashboard.vue";
 
 const routes = [
   {
@@ -19,12 +20,17 @@ const routes = [
     path: "/tenant",
     name: "tenant-dashboard",
     component: TenantDashboardView,
-  }
-]
+  },
+  {
+    path: "/superadmin",
+    name: "superadmin-dashboard",
+    component: SuperadminDashboardView,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
 
-export default router
+export default router;
